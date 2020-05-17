d08ccb56bd0d5e07cbaae7aaab48b20a68e8abaf
dinskid <39125542+dinskid@users.noreply.github.com>
2020-05-17 11:38:04 +0000

    Updates test/runtests.jl (#9)
    * Adds sudo permissions to travis CI
    
    * Update .travis.yml
    
    * Update .travis.yml
    
    * Update .travis.yml
    
    * Update test/runtests.jl
    
    * Update test/runtests.jl
    
    * Update test/runtests.jl
    
    * Update .travis.yml
    
    * Undo changes, to sync with main repo
    
    To undo the changes to sync with the main repo before making changes

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,8 +3,13 @@ using Test
 
 include("../src/diseaseProg.jl")
 # Install scikit-learn if not installed
-PyCall.pyimport_conda("numpy", "pandas", "scipy")
-PyCall.pyimport_conda( "dask", "cloudpickle", "distributed")
+PyCall.pyimport_conda("pandas", "pandas")
+PyCall.pyimport_conda("numpy", "numpy")
+PyCall.pyimport_conda("scipy", "scipy")
+PyCall.pyimport_conda("dask", "dask")
+PyCall.pyimport_conda("cloudpickle", "cloudpickle")
+PyCall.pyimport_conda("distributed", "distributed")
+PyCall.pyimport_conda("xlrd", "xlrd")
 
 py"""
 import os

fc98cda859a437626ddb2d2657f5c79a0acceb81
Chris Rackauckas <accounts@chrisrackauckas.com>
2020-06-07 12:39:20 -0400

    Accelerate by 36,000x and organize
    This accelerates the test case by 36,000x and organizes it. The high level scripts are pulled out of `src` and coexist is made into a proper package (note: the name of the module should change because Julia module conventions have a capital in the front!). Then, a code generation scheme via ModelingToolkit is setup as the `dydt_generator.jl`, which results in a 36,000x acceleration of the ODE solve. To demonstrate caching I show that the generated code can be saved to a file and then is benchmarked in `pyjulia_benchmark.jl`. However, note that saving to a file isn't required (as the `dydt_generator.jl` doesn't use the file generated version at all), so generated fast forms can be used on the fly if needed.
    
    Note that ModelingToolkit doesn't seem to be totally needed here, you can get a good portion (but not all) of the speedup by preallocating buffers and rewriting the einsum expressions. However, this just demonstrates a way to use the code generators such that 0 work is required to get the optimal expressions. The function that is generated is in a sparse form so as the networks get larger the amount of acceleration should keep increasing as well.
    
    Note that the only "glut" in the system here is that the symbolic simplification for generating the fastest form of the function takes around 80 seconds. But this is unnecessary since most of the values are zeros. We should open an issue about this an invite @yingboma and @shashi to the repo to take a look at some accelerated simplification tools for this case.

---
+++ scripts/dydt_generator.jl
@@ -0,0 +1,99 @@
+using ModelingToolkit
+using DifferentialEquations
+using Plots
+using BenchmarkTools
+using coexist
+
+# initial state
+nAge=9
+nHS=8
+nIso=4
+nI=4
+nTest=4
+param=(nAge=nAge, nHS=nHS, nIso=nIso, nTest=nTest)
+ssize=(nAge, nHS, nIso, nTest)
+
+function dydt_Complete(stateTensor_flattened,p,t)
+	T = eltype(stateTensor_flattened)
+	stateTensor = reshape(stateTensor_flattened, (nTest, nIso, nHS, nAge))
+	dydt = zeros(T,size(stateTensor)...)
+	trTensor_complete = zeros(T,(nTest, nIso, nHS, nTest, nIso, nHS, nAge))
+	trTensor_diseaseProgression = coexist.trFunc_diseaseProgression(;param...)
+	for k in 1:4
+		shape = size(trTensor_diseaseProgression[:,k,:,:])
+		expand_dims = reshape(trTensor_diseaseProgression[:,k,:,:],
+					(shape[1:end-2]..., 1, shape[end-1:end]...)) # equal to np.exapand_dims
+		# slice = trTensor_complete[:,k,:,:,k,:,:]
+		# @einsum view[m,l,j,i] := slice[l,m,l,j,i]
+		view = coexist.einsum("ijlml->ijlm", trTensor_complete[:,k,:,:,k,:,:],T)
+		view .+= expand_dims
+		trTensor_complete[:,k,:,:,k,:,:] = coexist._einsum12(trTensor_complete[:,k,:,:,k,:,:], view, T)
+		# @einsum slice[l,m,l,j,i] = view[m,l,j,i]
+		# trTensor_complete[:,k,:,:,k,:,:] = slice
+	end
+
+	# @einsum to_be_modified[l,k,j,i] := trTensor_complete[l,k,j,l,k,j,i]
+	to_be_modified = coexist.einsum("ijkljkl->ijkl", trTensor_complete, T)
+	to_be_modified .-= coexist.einsum("...jkl->...", trTensor_complete, T)
+	# @einsum trTensor_complete[l,k,j,l,k,j,i] = view[l,k,j,i]
+	trTensor_complete = coexist._einsum11(trTensor_complete, to_be_modified, T)
+
+
+	dydt = coexist.einsum("ijkl,ijklmnp->imnp", stateTensor, trTensor_complete, T)
+	return reshape(dydt, prod(size(dydt)))
+end
+
+state = 50*ones(9*8*4*4)
+dstate = similar(state)
+
+using ModelingToolkit
+@variables t u[1:1152](t)
+@derivatives D'~t
+
+dydt_symbolic = dydt_Complete(u,0,nothing)
+dydt_symbolic = simplify.(dydt_symbolic)
+sys = ODESystem(D.(u) .~ dydt_symbolic,t,u,[])
+computed_f = generate_function(sys)[2]
+
+open(joinpath(@__DIR__,"..","src","generated_dydt.jl"), "w") do io
+   write(io, "const dydt = $computed_f")
+end
+
+f = eval(computed_f)
+
+function solveSystem_unoptimized(
+	state,
+	timeSpan = (0.0, 80.0),
+	p=nothing
+	)
+	prob = ODEProblem(dydt_Complete,state,(0.0,80.0),p=nothing)
+	sol = solve(prob,Tsit5(),reltol=1e-3,abstol=1e-3)
+	sol = convert(Array, sol)
+	return reshape(sol, (4,4,8,9, Int64(prod(size(sol))/(4*4*8*9))))
+end
+
+function solveSystem(
+	state,
+	timeSpan = (0.0, 80.0),
+	p=nothing
+	)
+	prob = ODEProblem(f,state,(0.0,80.0),p=nothing)
+	sol = solve(prob,Tsit5(),reltol=1e-3,abstol=1e-3)
+	sol = convert(Array, sol)
+	return reshape(sol, (4,4,8,9, Int64(prod(size(sol))/(4*4*8*9))))
+end
+
+using BenchmarkTools
+dydt_benchmark = @benchmark dydt_Complete(state,0,nothing)
+f_benchmark = @benchmark f(dstate,state,0,nothing)
+solveSystem_unoptimized_benchmark = @benchmark solveSystem_unoptimized(state)
+solveSystem_benchmark = @benchmark solveSystem(state)
+
+println("BENCHMARK OF f")
+display(dydt_benchmark)
+display(f_benchmark)
+println()
+println("BENCHMARK OF solveSystem")
+display(solveSystem_unoptimized_benchmark)
+display(solveSystem_benchmark)
+println()
---
+++ scripts/pyjulia_benchmark.jl
@@ -0,0 +1,40 @@
+using coexist, PyCall
+coexist.pycoexist_setup()
+
+pydir = joinpath(@__DIR__,"..", "coexist_python", "model_COVID_testing-ODEoutput.py")
+py"""
+import os
+exec(open(os.path.join($pydir)).read())
+"""
+
+state = 50*ones(9*8*4*4)
+dstate = similar(state)
+
+function solveSystem(
+	state,
+	timeSpan = (0.0, 80.0),
+	p=nothing
+	)
+	prob = ODEProblem(coexist.dydt,state,(0.0,80.0),p=nothing)
+	sol = solve(prob,Tsit5(),reltol=1e-3,abstol=1e-3)
+	sol = convert(Array, sol)
+	return reshape(sol, (4,4,8,9, Int64(prod(size(sol))/(4*4*8*9))))
+end
+
+dydt_benchmark = @benchmark coexist.dydt(dstate,state,0,nothing)
+solveSystem_benchmark = @benchmark solveSystem(state)
+py_dydt_benchmark = @benchmark py"solveSystem(state, 80, **paramDict_current)"
+py_solveSystem_benchmark = @benchmark py"solveSystem(state, 80, **paramDict_current)"
+
+println("BENCHMARK OF dydt_Complete")
+display(dydt_benchmark)
+println()
+println("BENCHMARK OF solveSystem")
+display(solveSystem_benchmark)
+println()
+println("BENCHMARK OF py_dydt_Complete")
+display(py_dydt_benchmark)
+println()
+println("BENCHMARK OF py_solveSystem")
+display(py_solveSystem_benchmark)
+println()
